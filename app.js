546d94e2861afc6793e60be8348f3ad64157498b
pendulum <75574213+shkvik@users.noreply.github.com>
2022-12-21 21:37:15 +0300

    add static files

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const express = require("express");
+const handlebars = require('express-handlebars');
+const hbs = require('hbs')
 const mysql = require("mysql2");
-  
+
+
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -16,13 +21,26 @@ connection.connect(function(err){
     }
  });
 
-const express = require("express");
+
+ 
 const app = express();
 const userRouter = require("./routes/userRouter.js");
 const homeRouter = require("./routes/homeRouter.js");
- 
-app.set("view engine", "hbs");
+
+
+
+app.engine(
+    'hbs',
+    handlebars.engine({
+      layoutsDir: 'views/layout',
+      defaultLayout: 'layout',
+      extname: 'hbs',
+    })
+  )
+
+
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
  
 app.use("/users", userRouter);
 app.use("/", homeRouter);
@@ -31,4 +49,8 @@ app.use(function (req, res, next) {
     res.status(404).send("Not Found");
 });
  
-app.listen(3000, ()=>console.log("Сервер запущен и ожидает подключения..."));
+app.listen(3000, ()=> { console.log("Сервер запущен и ожидает подключения...") });
+
+
+var ip = require("ip");
+console.log(`main url: http://${ip.address()}:3000`);
