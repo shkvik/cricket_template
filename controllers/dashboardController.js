818ecefb077a64e3732ce447edf81ac2c7230f96
pendulum <75574213+shkvik@users.noreply.github.com>
2023-01-11 20:13:51 +0300

    test frontend

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -8,6 +8,67 @@ exports.index = function (request, response){
     response.render("../views/dashboard.hbs", 
     {
         Title: dashboard.Title,
-        Arr: ['Пидор', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
+        Temperature: 
+        [
+            {
+                mounth: 'January',
+                value: 26
+            },
+            {
+                mounth: 'February',
+                value: 39
+            },
+            {
+                mounth: 'March ',
+                value: 36
+            },
+            {
+                mounth: 'April ',
+                value: 30
+            },
+            {
+                mounth: 'May ',
+                value: 19
+            },
+        ],
+        Humidity: 
+        [
+            {
+                hour: '00:00',
+                value: 80
+            },
+            {
+                hour: '03:00',
+                value: 70
+            },
+            {
+                hour: '06:00',
+                value: 60
+            },
+            {
+                hour: '09:00',
+                value: 50
+            },
+            {
+                hour: '12:00',
+                value: 40
+            },
+            {
+                hour: '15:00',
+                value: 50
+            },
+            {
+                hour: '18:00',
+                value: 60
+            },
+            {
+                hour: '21:00',
+                value: 70
+            },
+            {
+                hour: '24:00',
+                value: 90
+            },
+        ]
     });
 };
--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,21 +1,111 @@
 
 const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: values,
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
+new Chart(ctx, 
+{
+    type: 'line',
+    data: 
+    {
+        labels: temperature_mounths,
+        datasets: 
+        [
+            {
+                data: temperature_values,
+                borderWidth: 1
+            }
+        ]
     },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+    options:
+    {
+        scales: 
+        {
+            y: 
+            {
+                max: 100,
+                min: 0
+            }
+        },
+        plugins:
+        {
+            legend: 
+            {
+                display: false
+            }
         }
-      }
+
+    }
+});
+
+const ctx2 = document.getElementById('myChart2');
+
+new Chart(ctx2, 
+{
+    type: 'line',
+    data: 
+    {
+        labels: humidity_hours,
+        datasets: 
+        [
+            {
+                data: Humidity_values,
+                borderWidth: 1
+            }
+        ]
+    },
+    options:
+    {
+        scales: 
+        {
+            y: 
+            {
+                max: 100,
+                min: 0
+            }
+        },
+        plugins:
+        {
+            legend: 
+            {
+                display: false
+            }
+        }
+
+    }
+});
+
+const ctx3 = document.getElementById('myChart3');
+
+new Chart(ctx3, 
+{
+    type: 'line',
+    data: 
+    {
+        labels: humidity_hours,
+        datasets: 
+        [
+            {
+                data: Humidity_values,
+                borderWidth: 1
+            }
+        ]
+    },
+    options:
+    {
+        scales: 
+        {
+            y: 
+            {
+                max: 100,
+                min: 0
+            }
+        },
+        plugins:
+        {
+            legend: 
+            {
+                display: false
+            }
+        }
+
     }
 });
