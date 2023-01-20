c95ff444b2efec14aaf1a79d21e1801bfd2df7aa
pendulum <75574213+shkvik@users.noreply.github.com>
2023-01-21 00:59:47 +0300

    i tired

--- services/database/index.js
+++ services/database/index.js
@@ -1,20 +1,10 @@
 const mySqlProvider = require("./mySqlProvider")
-//const models = require('./UserTest')
-
 
 mySqlProvider.sequelize.sync().then(result=>{
     console.log(result);
 }).catch(err=> console.log(err));
     
-// const tryThis = async() => {
-//     return models.User.findAll({raw:true}).then(users=>{
-//         console.log(users);
-//     }).catch(err=>console.log(err));
-// }
-
-// tryThis();
 
 module.exports = {
-    test: "chek",
     provider: mySqlProvider
 }
--- services/database/mySqlProvider.js
+++ services/database/mySqlProvider.js
@@ -1,7 +1,6 @@
 const {Sequelize} = require("sequelize");
 
 
-
 module.exports = {
 
   sequelize: new Sequelize("world", "root", "1337", {
@@ -9,15 +8,5 @@ module.exports = {
     host: "localhost",
     define: {timestamps: false}    
   })
-  
-}
 
-
-// sequelize.sync().then(result=>{
-//     console.log(result);
-//     }).catch(err=> console.log(err));
-
-
-// User.findAll({raw:true}).then(users=>{
-//     console.log(users);
-// }).catch(err=>console.log(err))
+};
